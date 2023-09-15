bf8c5796c70460077d2403754adfc28ede7a36c3
kawtar el bakkali <kawtarelbakkali3@gmail.com>
2023-09-15 15:47:02 -0700

    the numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+
+	int i = 0;
+
+	do {
+		_putchar(i + 48);
+		i++;
+	} while (i >= 48 && i <= 57);
+	_putchar('\n');
+}
