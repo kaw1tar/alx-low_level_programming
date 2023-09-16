80c769d621b2635aac53d14af2c03668b0f0cbc7
kawtar el bakkali <kawtarelbakkali3@gmail.com>
2023-09-16 03:54:17 -0700

    triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -3,4 +3,29 @@
 /**
  * print_triangle - print a triangle useing _putchar
  *
+ * @size: size of the triangle
  *
+ * Return: Always 0 (success)
+ */
+
+void print_triangle(int size)
+{
+	int hght, base;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (hght = 1; hght <= size; hght++)
+		{
+			for (base = 1; base <= size; base++)
+			{
+				if ((hght + base) <= size)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
