ebf6c31bb1ab17b1fd9a070e98eb6e9ced442f9b
kawtar el bakkali <kawtarelbakkali3@gmail.com>
2023-09-18 11:18:22 -0700

    reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,16 +8,17 @@
 
 void rev_string(char *s)
 {
-	int l, i;
+	int l = 0, i = 0;
 	char c;
 
-	/*find string length without null char*/
-	for (l = 0; s[1] != '\0'; ++l)
-		;
-	for (i = 0; i < l / 2; i++)
+	while (s[i++])
+		l++;
+
+	for (i = l - 1; i >= l / 2; i--)
 	{
 		c = s[i];
-		s[i] = s[l - 1 - i];
-		s[l - 1 - i] = c;
+		s[i] = s[l - i - 1];
+		s[l - i - 1] = c;
 	}
 }
+
