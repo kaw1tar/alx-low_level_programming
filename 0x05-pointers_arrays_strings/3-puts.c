ce1265317590b37245f043ebcd7cb53155c7d88d
kawtar el bakkali <kawtarelbakkali3@gmail.com>
2023-09-18 09:54:52 -0700

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - print a string
+ *
+ * @str: string parameter
+ *
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		str++;
+
+	}
+	_putchar('\n');
+}
