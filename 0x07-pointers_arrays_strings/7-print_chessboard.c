b5267eaa8dcb2ed401443facf324a61214de6490
kawtar el bakkali <kawtarelbakkali3@gmail.com>
2023-09-25 15:59:45 -0700

    print chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print the cessboard
+ * @a: pointer of the borad
+ * Return: Always 0
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int x, y;
+
+	for (x = 0; x < 8; x++)
+	{
+		for (y = 0; y < 8; y++)
+		{
+			_putchar(a[x][y]);
+		}
+		_putchar('\n');
+	}
+}
