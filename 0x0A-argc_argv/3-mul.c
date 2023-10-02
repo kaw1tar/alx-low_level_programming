3ef0b824a13030cddf233b5bf94e4025b744257a
kawtar el bakkali <kawtarelbakkali3@gmail.com>
2023-10-02 16:10:55 -0700

    multiplies two num

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,14 +14,14 @@ int main(int argc, char *argv[])
 
 	result = atoi(argv[1]) * atoi(argv[2]);
 
-	if (argc == 3)
-	{
-		printf("%d\n", result);
-	}
-	else
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
+	}
+	else
+	{
+		printf("%d\n", result);
 	}
 	return (0);
 }
