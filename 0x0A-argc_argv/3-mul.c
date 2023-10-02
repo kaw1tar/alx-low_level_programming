357bf03ab8f1af7e39492a13cf5c1dc222cac843
kawtar el bakkali <kawtarelbakkali3@gmail.com>
2023-10-02 15:43:15 -0700

    multiplies two num

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: integer
+ * @argv: the pointer
+ * Return: 1 if the program does receive two arguments
+ */
+
+int main(int argc, char *argv[])
+{
+	int result;
+
+	result = atoi(argv[1]) * atoi(argv[2]);
+
+	if (argc == 3)
+	{
+		printf("%d\n", result);
+	}
+	else if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
