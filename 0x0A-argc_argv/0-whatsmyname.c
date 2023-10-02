cb585d6cf65c7d6c810fc0764afd59e7c8b77006
kawtar el bakkali <kawtarelbakkali3@gmail.com>
2023-10-02 13:15:57 -0700

    my new name is

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - printsthe nameof theprogram
+ * @argc: integer
+ * @argv: list
+ * Return: Always 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
