6ed45516b27a84c98ce609c4f96ab6874f550b39
kawtar el bakkali <kawtarelbakkali3@gmail.com>
2023-10-07 12:32:08 -0700

    concat 2 strings n

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,19 +23,22 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = " ";
 	if (s2 == NULL)
 		s2 = " ";
-
-	p = malloc(len1 + n + 1);
-	if (p == NULL)
-		return (NULL);
 	for (len1 = 0; s1[len1] != '\0'; len1++)
 		;
 	for (len2 = 0; s2[len2] != '\0'; len2++)
 		;
-	for (i = 0; p[i] != '\0'; i++)
+
+	p = malloc(len1 + n + 1);
+	if (p == NULL)
 	{
+		return (NULL);
+	}
+	for (i = 0; s1[i] != '\0'; i++)
 		p[i] = s1[i];
-		for (j = 0; j < n; j++)
-			p[i] = s2[j];
+	for (j = 0; j < n; j++)
+	{
+		p[i] = s2[j];
+		i++;
 	}
 	p[i] = '\0';
 
