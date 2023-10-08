b42c66cfb2727b820bda7c89afa4ae8d622fe15d
kawtar el bakkali <kawtarelbakkali3@gmail.com>
2023-10-08 10:28:15 -0700

    malloc array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,6 +3,23 @@
 #include "main.h"
 
 /**
+ * _memset - fill a block of memory with a specified value
+ * @s: pointer
+ * @i: constant value
+ * @n: max bytes to use
+ * Return: s
+ */
+
+char *_memset(char *s, char i, unsigned int n)
+{
+	char *pt = s;
+
+	while (n--)
+		*s++ = i;
+	return (pt);
+}
+
+/**
  * _calloc - allocates memory for an array
  * @nmemb: number of elements
  * @size: the size in bytes!
@@ -11,7 +28,7 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *p;
+	void *p;
 
 	if (nmemb == 0 || size == 0)
 	{
@@ -22,5 +39,7 @@ if (p == 0)
 {
 return (NULL);
 }
+_memset(p, 0, nmemb * size);
+
 return (p);
 }
