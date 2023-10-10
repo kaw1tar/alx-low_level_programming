3de072ba8c868354f9342c29ced078cbc32fb195
kawtar el bakkali <kawtarelbakkali3@gmail.com>
2023-10-10 12:36:43 -0700

    DOG &

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG
+#define DOG
+
+
+/**
+ * struct dog - represents 3 things of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: who is the owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
