272893a47912434b06b9d5177deb17e988026cde
kawtar el bakkali <kawtarelbakkali3@gmail.com>
2023-10-12 13:49:22 -0700

    i do not know howa can i do this task i do my best

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_all - prints anything
+ * @format: a list of types of args passed to the func
+ */
+
+void print_all(const char * const format, ...)
+{
+
