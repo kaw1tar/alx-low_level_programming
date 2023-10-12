679014c64d2f2bc0716c4e545bada0d612da79d9
kawtar el bakkali <kawtarelbakkali3@gmail.com>
2023-10-12 05:28:29 -0700

    task 0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,4 +1,12 @@
+#ifndef variadic_fun
+#define variadic_fun
+
+#include <stdio.h>
+#include <stdarg.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+#endif
