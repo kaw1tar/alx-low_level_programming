d22141d95cf4e378d363d777e1b2626193009890
kawtar el bakkali <kawtarelbakkali3@gmail.com>
2023-10-19 13:31:53 -0700

    task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -18,24 +18,23 @@ list_t *add_node_end(list_t **head, const char *str)
 	{
 		return (NULL);
 	}
-	
+
 	new_node = malloc(sizeof(list_t));
-	
+
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
-	
+
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
-	
-	new_node->len = strlen(str);
-	new_node->next = NULL;
-	
+
+	new_node->len = strlen(new_node->str);
+
 	if (*head == NULL)
 	{
 		*head = new_node;
